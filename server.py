bc8b1d45c9419650998057a8170a5d8a26e1b6b9
Toxa <toxarus286@gmail.com>
2018-10-19 13:17:24 +0000

    по фиксили баги

--- server.py
+++ server.py
@@ -5,18 +5,18 @@ def generate_links():
     with app.test_request_context():
         Toxa_the_great_link=url_for('hello_user',username='Toxa')
         index_link = url_for('index')
-        links = {'Toxa`s page':Toxa_the_great_link,'Index':index_link,}
+        links = (('Toxa`s page',Toxa_the_great_link),('Index',index_link),)
     return  links
 @app.route('/')
 def index():
-    with app.test_request_context():
-        link = url_for('hello_user', username='Toxa the God of PUBG')
-        return render_template('index.html',link=link)
+    links = generate_links()
+    return render_template('index.html',links=links)
+
 @app.route('/user/')
 @app.route('/user/<path:username>')
 def hello_user(username= None):
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]
-    return render_template('user.html', username=username, numbers=numbers)
+    links = generate_links()
+    return render_template('user.html', username=username, links=links)
 
 
 with app.test_request_context():

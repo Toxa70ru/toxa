510505a621ebccd824b73977cb8dc5e6adb19d15
Toxa <toxarus286@gmail.com>
2018-11-22 13:09:41 +0000

    Added 1st migration

---
+++ sql/1.sql
@@ -0,0 +1,41 @@
+CREATE TABLE `shop`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `price` INT UNSIGNED NULL,
+  `description` VARCHAR(255) NULL,
+  `category_id` INT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `shop`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `image_url` VARCHAR(255) NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `shop`.`product`
+ADD INDEX `fk_category_idx` (`category_id` ASC);
+;
+ALTER TABLE `shop`.`product`
+ADD CONSTRAINT `fk_category`
+  FOREIGN KEY (`category_id`)
+  REFERENCES `shop`.`category` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `shop`.`image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `url` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `shop`.`image`
+ADD COLUMN `product_id` INT(11) NULL DEFAULT NULL AFTER `url`,
+ADD INDEX `fk_product_idx` (`product_id` ASC);
+;
+ALTER TABLE `shop`.`image`
+ADD CONSTRAINT `fk_product`
+  FOREIGN KEY (`product_id`)
+  REFERENCES `shop`.`product` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
